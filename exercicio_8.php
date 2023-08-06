287c28b766df967a3435057478517944baa0f007
AdrianoSoriano <adriano_soriano@hotmail.com>
2023-08-06 08:07:07 -0300

    primeiro exercicio da aula 4

---
+++ exercicio_8.php
@@ -0,0 +1,26 @@
+<html>
+
+    <head>
+        <title>EXERCÍCIOS PROPOSTOS DA AULA 04</title>
+    </head>
+
+    <body>
+        <?php
+        echo "Exercicio 08";
+        echo "<BR><BR>";
+
+        echo "1- Termine o script abaixo, imprimindo todos os números entre \$numero1 e \$numero2.";
+        echo "<BR>";
+        echo "\$numero = 7;";
+        echo "<BR>";
+        echo "\$numero = 27;";
+        echo "<BR><BR>";
+
+        for ($x=8; $x<=27; $x++) {
+
+            echo $x . "<br>";
+        }
+        ?>
+    </body>
+
+</html>
