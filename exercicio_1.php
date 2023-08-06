e5154e4fe4342a2c95198b7d9cee54357521e404
AdrianoSoriano <adriano_soriano@hotmail.com>
2023-08-05 23:38:37 -0300

    primeiro exercicio criado

---
+++ exercicio_1.php
@@ -0,0 +1,27 @@
+<html>
+
+    <head>
+        <title>EXERCÍCIOS PROPOSTOS DA AULA 03</title>
+    </head>
+
+    <body>
+        <?php
+        echo "Exercicio 01";
+        echo "<BR><BR>";
+
+        echo "1- Termine o script abaixo, somando o valor das variáveis \$valor1 e \$valor2, 
+                e multiplicando o resultado pelo valor de \$valor1 exibindo o resultado na tela.";
+        echo "<BR>";
+        echo "\$valor1 = 2;";
+        echo "<BR>";
+        echo "\$valor2 = 7;";
+        echo "<BR><BR>";
+
+        $valor1 = 2;
+        $valor2 = 7;
+
+        echo "O resultado é:" . ($valor1 + $valor2) * $valor1;
+        ?>
+    </body>
+
+</html>
