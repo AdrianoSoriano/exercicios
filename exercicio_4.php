044926acc67cdea263263a771b25f19c177670e7
AdrianoSoriano <adriano_soriano@hotmail.com>
2023-08-05 23:52:27 -0300

    quarto exercicio criado

---
+++ exercicio_4.php
@@ -0,0 +1,30 @@
+<html>
+
+    <head>
+        <title>EXERCÍCIOS PROPOSTOS DA AULA 03</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Exercicio 04";
+            echo "<BR><BR>";
+        
+            echo "4- Termine o script abaixo, verificando se a variável \$numero é par ou ímpar e exiba na tela a
+                    mensagem “número par” ou “numero impar”.";
+            echo "<BR>";
+            echo "\$numero = 7;";
+            echo "<BR><BR>";
+        
+                $numero = 7;
+        
+                if ($numero % 2 == 0) {
+        
+                    echo "O número é Par";
+                } else {
+        
+                    echo "O número é Impar";
+                }
+        ?>
+    </body>
+
+</html>
