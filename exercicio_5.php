7f2c76f143bda6f7f23591d6ba7dd58910eb49a2
AdrianoSoriano <adriano_soriano@hotmail.com>
2023-08-05 23:53:21 -0300

    quinto exercicio criado

---
+++ exercicio_5.php
@@ -0,0 +1,33 @@
+<html>
+
+    <head>
+        <title>EXERCÍCIOS PROPOSTOS DA AULA 03</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Exercicio 05";
+            echo "<BR><BR>";
+        
+            echo "5- Finalize o script abaixo verificando se valor da variável \$a é maior ou menor que o valor da variável
+                    \$b. A mensagem a ser impressa deve ser “A maior que B” ou “A menor que B”.";
+            echo "<BR>";
+            echo "\$a = 7;";
+            echo "<BR>";
+            echo "\$b = 21;";
+            echo "<BR><BR>";
+        
+                $a = 7;
+                $b = 21;
+        
+                if ($a > $b) {
+        
+                    echo "A maior que B";
+                } else {
+        
+                    echo "A menor que B";
+                }
+        ?>
+    </body>
+
+</html>
