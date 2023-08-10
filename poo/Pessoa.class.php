182e1e79434838f58a7de959282a616fd5c40dab
AdrianoSoriano <adriano_soriano@hotmail.com>
2023-08-10 18:50:52 -0300

    criação da classe pessoa

---
+++ poo/Pessoa.class.php
@@ -0,0 +1,26 @@
+<?
+class Pessoa {
+    //Atributos
+    public string $nome;
+    public int $idade;
+    public string $sexo;
+    public float $altura;
+    public int $peso;
+
+    //Metodos
+    public function setNome(string $nome): void {
+        $this->nome = $nome;
+    }
+    public function setIdade(int $idade): void {
+        $this->idade = $idade;
+    }
+    public function setSexo(string $sexo): void {
+        $this->sexo = $sexo;
+    }
+    public function setAltura(float $altura): void {
+        $this->altura = $altura;
+    }
+    public function setPeso(int $peso): void {
+        $this->peso = $peso;
+    }
+}
